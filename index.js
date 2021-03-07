6b89280aebf861f8261e423f722e76bda354ec7a
fluffyhake <haakon.reppen@gmail.com>
2021-03-07 15:42:36 +0100

    added inefficent example of serving json or html

--- index.js
+++ index.js
@@ -16,9 +16,31 @@ const fs = require('fs');
 
 const server = http.createServer ((req,res) => {
 
+    // Commented out due to being ineficcent
+    // console.log(req.url)
+    // if(req.url ==='/'){
+    //     fs.readFile(
+    //     path.join(__dirname, 'public', 'main.html'), 
+    //     (err, content) => {
+    //         if(err) throw err;
+
+    //         res.writeHead(200, { 'Content-Type': 'text/html'});
+    //         res.end(content);
+
+    //     })
+    // }
+    // if(req.url ==='/api/hostlist'){
+    //     const hosts = [
+    //         {name: "plex", ip: "192.168.55.?"}
+    //     ];
+    //     res.writeHead(200, {'Content-Type': 'application/json'});
+    //     res.end(JSON.stringify(hosts))
+    // }
 
 
-})
+
+
+});
 
 const PORT = process.env.PORT || 5000;
 
